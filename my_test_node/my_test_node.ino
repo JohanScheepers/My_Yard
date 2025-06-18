41a54356dec7fe479d6b857a833e31d379f2d100
JohanScheepersf <scheepersjohanf@gmail.com>
2025-06-18 10:35:49 +0200

    Add my_test_node.ino

---
+++ my_test_node/my_test_node.ino
@@ -0,0 +1,19 @@
+/**
+ * @file my_test_node.ino
+ * @brief Basic sketch for My Test Node.
+ *
+ * This is a placeholder sketch. You can add your ESP32/Arduino code here.
+ */
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println("My Test Node - Setup Complete");
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    delay(1000);
+}
