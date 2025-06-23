1069a0e8030351a826f8f76018df5ae17cbdd9fd
JohanScheepersf <scheepersjohanf@gmail.com>
2025-06-23 16:25:41 +0200

    Updates

--- my_yard/lib/src/features/home/application/home_screen_notifier.g.dart
+++ my_yard/lib/src/features/home/application/home_screen_notifier.g.dart
@@ -1,27 +1,26 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'config_list_notifier.dart';
+part of 'home_screen_notifier.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$configListNotifierHash() =>
-    r'2d158d3504fbeb9668e6fba7a49ed5460725a9b5';
+String _$selectedIndexHash() => r'cdd948bb31a5a6f1d6b09861233cec6667f5a0a4';
 
-/// See also [ConfigListNotifier].
-@ProviderFor(ConfigListNotifier)
-final configListNotifierProvider =
-    AsyncNotifierProvider<ConfigListNotifier, List<DeviceData>>.internal(
-  ConfigListNotifier.new,
-  name: r'configListNotifierProvider',
+/// See also [SelectedIndex].
+@ProviderFor(SelectedIndex)
+final selectedIndexProvider =
+    AutoDisposeNotifierProvider<SelectedIndex, int>.internal(
+  SelectedIndex.new,
+  name: r'selectedIndexProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$configListNotifierHash,
+      : _$selectedIndexHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$ConfigListNotifier = AsyncNotifier<List<DeviceData>>;
+typedef _$SelectedIndex = AutoDisposeNotifier<int>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
